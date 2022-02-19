f44de59503a3fd859b6b8c5644555bb4ba2eaf60
ok <30568743+Lock1@users.noreply.github.com>
2022-02-20 03:02:55 +0700

    Penambahan onmousemove
    Penambahan testing untuk onmousemove event

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,14 @@
+var vertices = [
+    0.0, 0.0,
+    0.5, 0.5
+];
+
+var colors   = [
+    0.0, 0.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0
+];
+
+
 function main() {
     var canvas  = document.getElementById("c");
     var gl      = canvas.getContext("webgl");
@@ -5,16 +16,6 @@ function main() {
 
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
     var colorAttributeLocation    = gl.getAttribLocation(program, "a_color");
-
-    var vertices = [
-        0.0, 0.0,
-        0.5, 0.5
-    ];
-
-    var colors   = [
-        0.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0
-    ];
 
     var positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -32,12 +33,22 @@ function main() {
         gl.STATIC_DRAW
     );
 
+    // Keyword referensi : JS mouse event handler
     canvas.onmousedown = function (e) { click_handler(e, gl, canvas) };
+    canvas.onmousemove = function (e) { hover_handler(e, gl, canvas) }; // TODO : Extra, Bonus
 
     drawScene();
 
     function drawScene() {
         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+        // Updating vertex buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array(vertices),
+            gl.STATIC_DRAW
+        );
 
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
@@ -68,19 +79,39 @@ function main() {
         var offset = 0;
         var count = 2;
         gl.drawArrays(primitiveType, offset, count);
+        window.requestAnimationFrame(drawScene);
+    }
+
+    function click_handler(e, gl, canvas) {
+        var x    = e.clientX;
+        var y    = e.clientY;
+        var rect = e.target.getBoundingClientRect();
+
+        // Normalisasi antara 0 - 1
+        x = (2*(x - rect.left) - canvas.width) / canvas.width;
+        y = (canvas.height - 2*(y - rect.top)) / canvas.height;
+
+        console.log(x, y);
+    }
+
+    function hover_handler(e, gl, canvas) {
+        var x    = e.clientX;
+        var y    = e.clientY;
+        var rect = e.target.getBoundingClientRect();
+
+        // Normalisasi antara 0 - 1
+        x = (2*(x - rect.left) - canvas.width) / canvas.width;
+        y = (canvas.height - 2*(y - rect.top)) / canvas.height;
+
+        vertices[2] = x;
+        vertices[3] = y;
+
+        // console.log(x, y);
+        drawScene();
     }
 }
 
-function click_handler(e, gl, canvas) {
-    var x    = e.clientX;
-    var y    = e.clientY;
-    var rect = e.target.getBoundingClientRect();
 
-    // Normalisasi antara 0 - 1
-    x = (2*(x - rect.left) - canvas.width) / canvas.width;
-    y = (canvas.height - 2*(y - rect.top)) / canvas.height;
 
-    console.log(x, y);
-}
 
 main();
